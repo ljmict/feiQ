4061b4dcb9237d7cc5898e2b430e6ba2674f60c4
ljmict <ljmict@163.com>
2018-11-25 19:03:57 +0800

    配置文件

---
+++ config/FeiQCoreData.go
@@ -0,0 +1,17 @@
+package config
+
+import "net"
+
+var UDPSocket *net.UDPConn = nil
+
+
+const (
+	FeiQPort     = "2425"
+	FeiQVersion  = "1"
+	FeiQUserName = "michael"
+	FeiQHostName = "mac-pro"
+	Broadcast    = "255.255.255.255"
+	IPMSGBrEntry = 0x00000001 //上线提醒消息命令
+	IPMSGBrEXIT  = 0x00000002 //下线提醒消息命令
+	IPMSGSendMsg = 0x00000020 //表示发送消息
+)
